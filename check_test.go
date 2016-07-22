8a7223ed4b89567f5e763a1245b85c61df8263bf
Jeff Hodges <jeff@somethingsimilar.com>
2016-07-21 23:20:37 -0700

    add tests and travis ci config

---
+++ check_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGoldenPath(t *testing.T) {
+	sites, fset, err := check("./testdata", 16, 8, 8)
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+	b := &bytes.Buffer{}
+	printSites(sites, fset, b)
+	actual := string(b.Bytes())
+	if goldenData != actual {
+		t.Errorf("output doesn't match, want:\n%s\n=============\ngot:\n%s", goldenData, actual)
+	}
+}
+
+const goldenData = `testdata/inner.go:24:6: parameter 'f' at index 0 should be made into a pointer (func CallsFoo(f Foo))
+testdata/inner.go:28:14: receiver, and parameter 'o' at index 0 should be made into pointers (func (Foo).OnOtherToo(o other))
+testdata/inner.go:32:16: receiver should be made into a pointer (func (other).OnStruct())
+testdata/inner.go:35:16: receiver should be made into a pointer (func (other).OnStruct2())
+`
